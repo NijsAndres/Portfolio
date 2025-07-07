d938e8c0c9f1d04835a1be5f575befbaa7090b0a
NijsAndres <andres.nijs@student.howest.be>
2025-07-07 10:45:19 +0200

    feat: setup done

---
+++ js/app.js
@@ -0,0 +1,23 @@
+// #region ***  DOM references                           ***********
+// #endregion
+
+// #region ***  Callback-Visualisation - show___         ***********
+// #endregion
+
+// #region ***  Callback-No Visualisation - callback___  ***********
+// #endregion
+
+// #region ***  Data Access - get___                     ***********
+// #endregion
+
+// #region ***  Event Listeners - listenTo___            ***********
+// #endregion
+
+// #region ***  Init / DOMContentLoaded                  ***********
+
+const init = () => {
+    console.log('DOM loaded');
+}
+
+document.addEventListener('DOMContentLoaded',init);
+// #endregion
